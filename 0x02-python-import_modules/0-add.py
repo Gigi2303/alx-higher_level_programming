443d61cd31271cc85580cc7f07d8431b0b9aad0a
Gigi2303 <ajibola.grace201@gmail.com>
2023-11-30 18:00:28 -0800

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
+    from _addit import add
 
     a = 1
     b = 2
---
+++ 0x02-python-import_modules/_addit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def _addit(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
