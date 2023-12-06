5153479a6f5c8d3d60af7cfff0df15553d465c0e
Gigi2303 <ajibola.grace201@gmail.com>
2023-12-06 00:21:51 -0800

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    11 = len(tuple_a)
-    12 = len(tuple_b)
-    avalue_1 = tuple_a[0] if 11 > 0 else 0
-    avalue_2 = tuple_a[1] if 11 > 1 else 0
-    bvalue_1 = tuple_b[0] if 12 > 0 else 0
-    bvalue_2 = tuple_b[1] if 12 > 1 else 0
+    t1 = len(tuple_a)
+    t2 = len(tuple_b)
+    avalue_1 = tuple_a[0] if t1 > 0 else 0
+    avalue_2 = tuple_a[1] if t1 > 1 else 0
+    bvalue_1 = tuple_b[0] if t2 > 0 else 0
+    bvalue_2 = tuple_b[1] if t2 > 1 else 0
 
     return ("{}".format((avalue_1 + bvalue_1, avalue_2 + bvalue_2)))
