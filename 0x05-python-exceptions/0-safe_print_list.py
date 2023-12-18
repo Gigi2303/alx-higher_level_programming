ca30bc35002d1187de160807fab637142fe20f22
Gigi2303 <ajibola.grace201@gmail.com>
2023-12-18 15:49:56 -0800

    aa

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,13 +4,12 @@ def safe_print_list(my_list=[], x=0):
     printed_length = 0
 
     try:
-        for g in range(x):
+        while printed_length < x:
             print("{}".format(my_list[printed_length]), end="")
             printed_length += 1
     except IndexError:
         pass
     
-    finally:
         print("")
 
         return printed_length
