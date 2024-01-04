a9e8ddda1452369623dfebc976241d55865cee2e
Gigi2303 <ajibola.grace201@gmail.com>
2024-01-04 15:24:51 -0800

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,11 @@
 #!usr/bin/python3
 
+
+"""
+A module that contains an empty Rectangle class that defines
+a rectangle
+"""
+
 class Rectangle():
+    """This is a class that defines a rectangle"""
     pass
