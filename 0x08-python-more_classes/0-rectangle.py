d9da97c14917e363d7959d7ddcab01d25cf75bfe
Gigi2303 <ajibola.grace201@gmail.com>
2024-01-04 15:32:11 -0800

    00

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,9 +2,10 @@
 
 
 """
-A module that contains an empty Rectangle class that defines
+This is a module that contains an empty Rectangle class that defines
 a rectangle
 """
+
 
 class Rectangle():
     """This is a class that defines a rectangle"""
