372da2c0a0d90f5dc671f767aa647e22a610fc89
Gigi2303 <ajibola.grace201@gmail.com>
2024-03-28 16:47:15 -0700

    corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ This module Fetches the status from https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-     url = 'https://alx-intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
 
     with urllib.request.urlopen(url) as response:
         data = response.read().decode('utf-8')
